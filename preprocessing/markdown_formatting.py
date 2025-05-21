a21cedb390c1db08d0b3369a0bb4ccfc73797e99
king-swim <sooyoung1118@gmail.com>
2025-05-21 18:52:40 +0900

    Feat: markdown formating preprocessing
    주어진 텍스트(답안, 정답 등)를 마크다운 형식으로 형식 변환 함수

---
+++ preprocessing/markdown_formatting.py
@@ -0,0 +1,35 @@
+import langchain
+import os
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("SECRET_KEY")
+
+
+def markdown_formatting(text):
+    template = """- Fix typos
+    - Convert the given content to markdown format in korean
+    - Use # for the entire article, ## the medium topic, and ### for the small topic.
+    - The article is an answer to a test and should not be added to or deleted.
+
+    <content>:
+    {content}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    model = ChatOpenAI(model="gpt-4.1-nano-2025-04-14", temperature=0)
+    rag_chain = RunnablePassthrough() | prompt | model | StrOutputParser()
+    result = rag_chain.invoke({"content": text})
+    return result
+
+
+if __name__ == "__main__":
+    text_path = "../dataset/판구조론 정립 과정 교안_extracted_text.txt"
+    with open(text_path, "r", encoding="utf-8") as f:
+        text = f.read()
+    result = markdown_formatting(text=text)
+    print("포맷팅 성공!\n결과:\n" + result)
