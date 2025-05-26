492815e914177bfb8c3d12d7b38cdb655633d865
king-swim <sooyoung1118@gmail.com>
2025-05-26 14:16:05 +0900

    Update main.py

--- langchain/main.py
+++ langchain/main.py
@@ -1,74 +1,25 @@
-# Step 0: Set env
-import json
+"""
+교안 PDF를 입력 받았을 시:
+OCR -> markdown_formatting -> ChromaDB 저장 (answer_key)
+
+복습 음성을 입력 받았을 시:
+STT -> correct_typo -> ChromaDB 저장 (student_answer)
+
+채점시:
+answer_key, student_answer -> compare_evaluate -> ChromaDB 저장 (Feedback)
+"""
+
 import langchain
 import os
-
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 from langchain_teddynote import logging
-
+from dotenv import load_dotenv
+from chunk_evaluate import chunk_evaluate
 from signup import get_or_create_user_chromadb
 
 logging.langsmith("Beakji-evaluate")
-
-from dotenv import load_dotenv
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from chunk_evaluate import chunk_evaluate
-
 load_dotenv()
 persist_directory = os.getenv("PERSIST_DIRECTORY")
 dataset_directory = os.getenv("DATASET_DIRECTORY")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-
-
-# answer_key vector 불러오기 -> chunk 순서대로 정렬
-def load_answer_key_chunks(vectorstore: Chroma, subject: str, unit: str):
-    filtered_docs = vectorstore.get(
-        where={"$and": [{"subject": subject}, {"unit": unit}, {"type": "answer_key"}]}
-    )
-
-    sorted_docs = sorted(
-        zip(filtered_docs["documents"], filtered_docs["ids"]),
-        key=lambda x: int(x[1].split("_")[-1]),
-    )
-
-    sorted_chunks = [doc for doc, _ in sorted_docs]
-    return sorted_chunks
-
-
-def evaluate_all_chunks(vectorstore, subject, unit, sorted_chunks):
-    all_feedback = []
-
-    for chunk in sorted_chunks:
-        result = chunk_evaluate(vectorstore, subject, unit, chunk)
-        if result:
-            try:
-                parsed = json.loads(result)
-                all_feedback.append(parsed)
-            except json.JSONDecodeError:
-                print("⚠️ JSON 파싱 실패: 결과를 건너뜀")
-                continue
-
-    merged_feedback = {"missing": [], "incorrect": []}
-
-    for fb in all_feedback:
-        merged_feedback["missing"].extend(fb.get("missing", []))
-        merged_feedback["incorrect"].extend(fb.get("incorrect", []))
-
-    # 중복 제거
-    merged_feedback["missing"] = list(set(merged_feedback["missing"]))
-    merged_feedback["incorrect"] = list(set(merged_feedback["incorrect"]))
-
-    return merged_feedback
-
-
-if __name__ == "__main__":
-    user_id = "user123"
-    subject = "지구과학"
-    unit = "판구조론 정립 과정"
-
-    vectorstore = get_or_create_user_chromadb(user_id)
-    answer_key_chunks = load_answer_key_chunks(vectorstore, subject, unit)
-
-    print(answer_key_chunks)
-    result = evaluate_all_chunks(vectorstore, subject, unit, answer_key_chunks)
-    print(result)
